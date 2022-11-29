670f29eaefe4b021b6fda90c299fdb1dee4be2a9
gymcrush <140093956+gymcrush@users.noreply.github.com>
2022-11-29 14:07:52 +0000

    bridge

--- sample_request.py
+++ sample_request.py
@@ -19,3 +19,6 @@ def request_image(img):
 print('get sample image from rust-lang.org')
 sample_image_response = requests.get('http://rust-lang.org/logos/rust-logo-128x128-blk.png')
 img = base64.b64encode(sample_image_response.content).decode('utf-8')
+
+# request
+request_image(img)
