09deb1b2a56fe8c72291a56d04683b0f423c8a1d
gymcrush <140093956+gymcrush@users.noreply.github.com>
2023-02-09 18:37:27 +0000

    1.10.4

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,4 @@ use axum::{handler::post, Router, Json, AddExtensionLayer, extract::Extension};
 use serde::{Serialize, Deserialize};
 use serde_json::{json, Value};
 use std::net::SocketAddr;
-use std::sync
+use std::sync::Arc
