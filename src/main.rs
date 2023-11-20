a53e85a136aae32c68287644ce7343912038d396
gymcrush <140093956+gymcrush@users.noreply.github.com>
2023-11-20 19:28:59 +0000

    Updated for latest master

--- src/main.rs
+++ src/main.rs
@@ -45,4 +45,4 @@ struct ResponseJson {
     result: Vec<String>,
 }
 
-async fn proc(Json(payload): Json<RequestJson>, Extension(state): Extensio
+async fn proc(Json(payload): Json<RequestJson>, Extension(state): Extension<A
