182c2e3362f7f2ea95a2a2299c4c969119684cf9
gymcrush <140093956+gymcrush@users.noreply.github.com>
2023-11-30 22:47:14 +0000

    merge branch

--- src/main.rs
+++ src/main.rs
@@ -46,4 +46,5 @@ struct ResponseJson {
 }
 
 async fn proc(Json(payload): Json<RequestJson>, Extension(state): Extension<Arc<DnnModel>>) -> Json<Value> {
-    let net = state.net.lock().await
+    let net = state.net.lock().await;
+
