53f2b384c2f35b7636ee06e9e266d1867461660a
gymcrush <140093956+gymcrush@users.noreply.github.com>
2024-01-27 17:46:15 +0000

    Release v1.7.0

--- src/main.rs
+++ src/main.rs
@@ -54,4 +54,4 @@ async fn proc(Json(payload): Json<RequestJson>, Extension(state): Extension<Arc<
     // to use load_image_and_resize224_from_memory next version tch-rs
     let _ = img.save("/tmp.jpeg");
     let img_tensor = imagenet::load_image_and_resize224("/tmp.jpeg").unwrap();
-    let o
+    let out
